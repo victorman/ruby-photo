053e70871b02690a4ae6a68bead86b0c8c3e10ca
Victor Frandsen <frandsen@netapp.com>
2012-12-05 18:24:08 -0800

    has many

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,6 @@
 class Photo < ActiveRecord::Base
   attr_accessible :description, :filename, :thumbnail
+  has_many :slides
   validates_presence_of :filename
   scope :with_filetype_jpg, :conditions => "filename like '%.jpg'"
   scope :with_thumbnail, :conditions => "thumbnail not null and thumbnail != ''"
