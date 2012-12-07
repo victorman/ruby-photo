377e6ca4298d5e30aedd8b9ffa2a3357fd4a8b28
Victor Frandsen <frandsen@netapp.com>
2012-12-07 15:09:29 -0800

    add many to many relationship btw categories and photos. plus fixture

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
   attr_accessible :name, :parent_id
+  has_and_belongs_to_many :photos
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,7 @@
 class Photo < ActiveRecord::Base
   attr_accessible :description, :filename, :thumbnail
   has_many :slides
+  has_and_belongs_to_many :categories
   validates_presence_of :filename
   scope :with_filetype_jpg, :conditions => "filename like '%.jpg'"
   scope :with_thumbnail, :conditions => "thumbnail not null and thumbnail != ''"
