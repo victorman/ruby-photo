4aee73347f76bda6054f7709021f4252dc755d31
Victor Frandsen <frandsen@netapp.com>
2012-12-07 17:59:24 -0800

    added the plugin acts as list

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -2,4 +2,6 @@ class Slide < ActiveRecord::Base
   attr_accessible :photo_id, :position, :slideshow_id
   belongs_to :slideshow
   belongs_to :photo
+
+  acts_as_list :scope => "slideshow_id"
 end
--- db/migrate/20121207224832_create_categories_photos.rb
+++ db/migrate/20121207224832_create_categories_photos.rb
@@ -1,12 +1,12 @@
 class CreateCategoriesPhotos < ActiveRecord::Migration
-  def up
+  def self.up
     create_table :categories_photos, :id => false do |t|
       t.integer     :category_id
       t.integer     :photo_id
     end
   end
 
-  def down
+  def self.down
     drop_table :categories_photos
   end
 end
